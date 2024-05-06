407fbd44b7b3f0295c69c6e1eb59fb597c0265bc
maddie-eleanor9 <145574063+maddie-eleanor9@users.noreply.github.com>
2024-05-06 09:27:06 -0400

    5/6
    I set up some of the basics that I need to start working on my project and got used to using pygame again because it’s been a little while. I created boolean variables to indicate which screen should be showing, and started making my title screen but it isn’t completely finished yet.

--- main.py
+++ main.py
@@ -1,1 +1,56 @@
-print("Fish Dish Game")
+import pygame
+import random
+
+#setup pygame
+pygame.init()
+pygame.font.init()
+title_font = pygame.font.SysFont('Showcard Gothic', 30)
+pygame.display.set_caption("Fish Dish")
+
+#setup screen
+SCREEN_HEIGHT = 370
+SCREEN_WIDTH = 530
+size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode(size)
+
+#different screens
+start_screen = True
+level_1 = False
+level_2 = False
+win_screen = False
+lost_food = False
+lost_enemy = False
+
+#misc variables
+r = 24
+g = 56
+b = 99
+
+#rendering text
+title = "Fish Dish"
+display_title = title_font.render(title, True, (255, 255, 255))
+
+run = True
+#main program loop
+while run:
+
+    #event loop
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.MOUSEBUTTONUP and start_screen == True:
+            start_screen = False
+            level_1 = True
+
+    #blit sprites
+    screen.fill((r, g, b))
+    if start_screen == True:
+        screen.blit(display_title, (40, 40))
+    elif level_1 == True:
+        screen.fill((255, 0 ,0))
+    pygame.display.update()
+
+
+pygame.quit()
